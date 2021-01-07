0feb11d9dd504dbfa580b456d7d5760630ff9e0c
Mohamed Rouissi <61789098+aftermath8@users.noreply.github.com>
2021-01-07 13:51:00 +0100

    Add files via upload

---
+++ datenbanken/Übungsaufgabe2/Teil 2/uebungsaufgabe22-2.php
@@ -0,0 +1,33 @@
+<meta charset="UTF-8">
+<html>
+ <head>
+  <title>PHP-Test</title>
+ </head>
+ <header>
+   <style>
+   header
+   {
+     color: black;
+     background: lightblue;
+     padding : 10px;
+   }
+   </style>
+ <?php echo '<h1> Übungsaufgabe 2</h1>'; ?>
+</header>
+
+<br/>
+
+<footer>
+  <style>
+  footer
+  {
+    background: yellow;
+  }
+  </style>
+
+<?php
+$datum = date("d.m.Y H:i:s");
+echo "Aktueller Serverzeit ist : $datum";
+?>
+</footer>
+</html>
